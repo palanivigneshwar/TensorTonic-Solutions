f376356b13eee5e1acc4eb30015a0042ba9561bb
Palani Vigneshwar <palanivigneshwar@gmail.com>
2026-04-11 21:41:42 +0530

    Update solution for k-means-centroid-update

--- k-means-centroid-update/k-means-centroid-update.py
+++ k-means-centroid-update/k-means-centroid-update.py
@@ -1,18 +1,20 @@
+import numpy as np
 def k_means_centroid_update(points, assignments, k):
     """
     Compute new centroids as the mean of assigned points.
     """
     # Write code here
-    
-    centroid=[]
-    for i in range(k):
-        points_k=[points[j] for j in range(len(assignments)) if assignments[j]==i]
-        n=len(points_k)
-        if n!=0:
-            sum=[0 for j in range(len(points[0]))]
-            for point_k in points_k:
-                sum=[sum[j]+point_k[j] for j in range(len(points[0]))]
-            centroid.append([sums/n for sums in sum])
-        else:
-            centroid.append([0.0 for i in range(len(points[0]))])
-    return centroid
+    _,counts=np.unique(assignments,return_counts=True)
+    clusters=[i for i in range(k)]
+    left_overs=k-len(counts)
+    for i in range(left_overs):
+        counts=np.append(counts,0)
+    centroids=[]
+    print(counts)
+    for cluster,count in zip(clusters,counts):
+        point=[points[i] for i in range(len(assignments)) if assignments[i]==cluster]
+        centroid=list(np.sum(point,axis=0)/count) if count>0 else [0]*len(points[0])
+        print(centroid)
+        centroids.append(centroid)
+    print(centroids)
+    return centroids
