452e01687373951e09b3bcee5cc5a828be1ada8b
Palani Vigneshwar <palanivigneshwar@gmail.com>
2026-04-12 16:56:43 +0530

    Update solution for relu-activation

--- relu-activation/relu-activation.py
+++ relu-activation/relu-activation.py
@@ -5,7 +5,4 @@ def relu(x):
     Implement ReLU activation function.
     """
     # Write code here
-    if type(x)==int or type(x)==float:
-        x=[x]
-    x=np.asarray(x,dtype=float)
-    return np.where(x>=0.0,x,0.0)
+    return np.where(np.asarray(x)>0,x,0)
